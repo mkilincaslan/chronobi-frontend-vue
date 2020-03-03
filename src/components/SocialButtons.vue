80f6cd53f76e6e764a5277f190f2bed0b04e5ff7
sami dönmez <samidonmezdeveloper@gmail.com>
2020-03-03 21:31:47 +0300

    $1

--- src/components/SocialButtons.vue
+++ src/components/SocialButtons.vue
@@ -1,21 +1,21 @@
 <template>
   <ul class="list-inline social-buttons">
-            <li class="list-inline-item" >
+            <li class="list-inline-item" v-show="item.tw">
               <a :href="item.tw">
                 <i class="fab fa-twitter"></i>
               </a>
             </li>
-            <li class="list-inline-item">
+            <li class="list-inline-item" v-show="item.fb">
               <a :href="item.fb">
                 <i class="fab fa-facebook-f"></i>
               </a>
             </li>
-            <li class="list-inline-item">
+            <li class="list-inline-item" v-show="item.ln">
               <a :href="item.ln">
                 <i class="fab fa-linkedin-in"></i>
               </a>
             </li>
-            <li class="list-inline-item">
+            <li class="list-inline-item" v-show="item.in">
               <a :href="item.in">
                 <i class="fab fa-instagram"></i>
               </a>
